72f66c16b47952bf4c9848589bbad1832dd9603f
sailingnn <457886446@qq.com>
2022-04-25 14:18:12 +0800

    added frontend

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ morgan.format('tinymore', ':method :url :status :res[content-length] - :response
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan('tinymore'))
 
 let persons = [
